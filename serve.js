298a77327fc21b03cfcb2bafc1e13870cba0aafc
Jin Hengyu <inveta@qq.com>
2025-09-13 10:08:08 +0800

    Add circle WebGL demo and static file server
    Added a new WebGL-based circle visualization at html/circle.html with supporting favicon. Updated index.html to include the new demo. Introduced serve.js, a simple static HTTP server for serving HTML and PNG files.

---
+++ serve.js
@@ -0,0 +1,29 @@
+// 静态http文件服务
+
+
+
+const mimes = {
+  '.html': 'text/html',
+  '.png': 'image/png',
+}
+
+const http = require('http')
+const fs = require('fs').promises
+const server = http.createServer(async (req, res) => {
+
+  const filePath = __dirname + req.url;
+  const data = await fs.readFile(filePath).catch(err => err.message);
+  const ext = req.url.slice(req.url.lastIndexOf('.')).toLowerCase();
+  const mime = mimes[ext] || 'application/octet-stream';
+  res.writeHead(200, { 'Content-Type': mime });
+  res.end(data);
+
+});
+
+
+const PORT = 3001
+const os = require('os')
+const ip = os.networkInterfaces()['WLAN'].find(a => a.family === 'IPv4').address
+server.listen(PORT, () => console.table({
+  'server': `http://${ip}:${PORT}/index.html`,
+}));
